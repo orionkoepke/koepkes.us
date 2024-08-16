9dfe1f7419b1303f9f03d26579994edb7348da1f
Orion <orion.koepke@gmail.com>
2024-08-16 14:58:03 -0500

    Added Routing By Subdomain
    Added a way for the page to redirect users from <subdomain>.host to host/<subdomain>.

--- src/TestPage.js
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <div>
-        <h1>This is a test page!</h1>
-    </div>
-  );
-}
--- src/index.js
+++ src/index.js
@@ -5,24 +5,52 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Home from './Home';
 import ErrorPage from './ErrorPage';
-import TestPage from './TestPage';
+import OrionResume from './OrionResume/OrionResume';
 
-const router = createBrowserRouter([
-  {
-      path: "/",
-      element: <Home />,
+
+function getSubdomain(){
+  return window.location.host.split(".")[0];
+}
+
+function getUrlWithSubdomainAsPath(){
+  var location = window.location;
+  var subdomain = getSubdomain();
+  var newHostName = location.host.replace(`${subdomain}.`, '', 1);
+  return `${location.protocol}//${newHostName}/${subdomain}`;
+}
+
+function goToUrl(url){
+  console.log(`Redirecting from ${window.location.href} to ${url}`);
+  window.location.replace(url);
+}
+
+function render(){
+  const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        errorElement: <ErrorPage />
+    },
+    {
+      path: "/orion",
+      element: <OrionResume />,
       errorElement: <ErrorPage />
-  },
-  {
-    path: "/test",
-    element: <TestPage />,
-    errorElement: <ErrorPage />
-  }
-]);
+    }
+  ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
+
+
+switch(getSubdomain().toLowerCase()){
+  case "orion":
+    goToUrl(getUrlWithSubdomainAsPath());
+    break;
+  default:
+    render();
+}
